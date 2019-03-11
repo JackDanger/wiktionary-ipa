e278a79bf2dacc5cce702d3ed89ff43ed9209de2
Jack Danger <github@jackcanty.com>
2019-03-11 11:02:08 -0700

    using a future-proof URI

--- get-wiktionary.sh
+++ get-wiktionary.sh
@@ -1,2 +1,2 @@
-dump="https://dumps.wikimedia.org/enwiktionary/20170101/enwiktionary-20170101-pages-articles.xml.bz2"
+dump="https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-articles.xml.bz2"
 wget -qO- $dump | pv -s 560m| bunzip2 > enwik.xml
